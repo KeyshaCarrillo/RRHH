b882a08bf7bb3fb7909b6c886c1a1e04f2653f0d
lolkingjra <ja2159586@gmail.com>
2025-09-04 14:14:12 -0600

    Update Asalariado.java

--- HumanResources/src/modelo/utils/Asalariado.java
+++ HumanResources/src/modelo/utils/Asalariado.java
@@ -4,10 +4,15 @@ import modelo.inter.*;
 public class Asalariado extends Empleado implements Bonificable {
 
     private double salarioMensual;
-    //commit prueba 
+    private Incentivo politicaDeIncentivo;
+      public Asalariado(String cedula, String nombre, double salarioMensual) {
+        //super(cedula, nombre);
+        this.salarioMensual = salarioMensual;
+        this.politicaDeIncentivo = new IncentivoPorDesempeno(this.salarioMensual);
+    }
     @Override
     public double bono() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       return politicaDeIncentivo.calcularIncentivo(this);
     }
 
     @Override
