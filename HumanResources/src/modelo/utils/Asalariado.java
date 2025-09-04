d28f955beec489e8cd4f031221ee54f44e422e99
lolkingjra <ja2159586@gmail.com>
2025-09-04 13:51:34 -0600

    commitprueba

--- HumanResources/src/modelo/utils/Asalariado.java
+++ HumanResources/src/modelo/utils/Asalariado.java
@@ -4,7 +4,7 @@ import modelo.inter.*;
 public class Asalariado extends Empleado implements Bonificable {
 
     private double salarioMensual;
-
+    //commit prueba 
     @Override
     public double bono() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
